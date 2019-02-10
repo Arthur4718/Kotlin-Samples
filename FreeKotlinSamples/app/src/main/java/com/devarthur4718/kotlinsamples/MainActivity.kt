cb024a2dd0ee7c608cfcef203df3db75b7287764
Unknown <arthur.gomes_4718@hotmail.com>
2019-02-10 21:52:35 -0200

    Splash Screen

--- FreeKotlinSamples/app/src/main/java/com/devarthur4718/kotlinsamples/MainActivity.kt
+++ FreeKotlinSamples/app/src/main/java/com/devarthur4718/kotlinsamples/MainActivity.kt
@@ -1,12 +1,29 @@
 package com.devarthur4718.kotlinsamples
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        setContentView(R.layout.activity_main)
+
+        //Passando para a próxima tela
+
+        Handler().postDelayed({
+
+            startActivity(Intent(this, MainActivity::class.java))
+
+            finish()
+
+        }, 2000)
+
     }
 }
