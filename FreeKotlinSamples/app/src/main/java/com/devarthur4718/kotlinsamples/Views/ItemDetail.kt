58eaaaeb88a8fd9ecae69ee8f139475cc8d91f96
Unknown <arthur.gomes_4718@hotmail.com>
2019-03-05 16:22:43 -0300

    Backup

--- FreeKotlinSamples/app/src/main/java/com/devarthur4718/kotlinsamples/Views/ItemDetail.kt
+++ FreeKotlinSamples/app/src/main/java/com/devarthur4718/kotlinsamples/Views/ItemDetail.kt
@@ -1,7 +1,6 @@
 package com.devarthur4718.kotlinsamples.Views
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity;
 import com.devarthur4718.kotlinsamples.R
 
