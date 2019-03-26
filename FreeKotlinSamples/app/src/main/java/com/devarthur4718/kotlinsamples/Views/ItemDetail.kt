3939ed864058ce7df5e8fe313fbafc6e457076cc
Unknown <arthur.gomes_4718@hotmail.com>
2019-03-25 22:14:03 -0300

    String array.

--- FreeKotlinSamples/app/src/main/java/com/devarthur4718/kotlinsamples/Views/ItemDetail.kt
+++ FreeKotlinSamples/app/src/main/java/com/devarthur4718/kotlinsamples/Views/ItemDetail.kt
@@ -1,9 +1,12 @@
 package com.devarthur4718.kotlinsamples.Views
 
+import android.content.res.Resources
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 import com.devarthur4718.kotlinsamples.R
+import java.util.*
 
 
 class ItemDetail : AppCompatActivity() {
@@ -13,14 +16,21 @@ class ItemDetail : AppCompatActivity() {
         setContentView(R.layout.activity_item_detail)
 
 
-        initAction()
+
         setTheme(R.style.AppTheme)
         setSupportActionBar(findViewById(R.id.my_toolbar))
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
+
+        initAction()
+
+
+
     }
 
     private fun initAction() {
+
+      
 
 
     }
